aafbccf30529982757b800b8349527f2b252df30
unknown <pallabipakira106@gmail.com>
2025-12-01 20:09:36 +0530

    remove padding

--- js/index.js
+++ js/index.js
@@ -52,3 +52,49 @@ document.getElementById("subscribeButton").addEventListener("click", () => {
 // const track = document.getElementById("#logoTrack");
 // const logos = track.innerHTML;
 // track.innerHTML += logos;
+
+$(document).ready(function () {
+  $(".testimonial-slider").slick({
+    centerMode: true,
+    centerPadding: "0",
+    slidesToShow: 3,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
